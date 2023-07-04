606e47de3560706529ff9fbd2d188284add180a2
Paul Abelardo Cuichan Ayo <pcuichan@mag.gob.ec>
2023-07-04 15:30:48 -0500

    fix handler error

--- src/main/java/com/hyundai/challenge/configuration/handler/error/ApplicationExceptionHandler.java
+++ src/main/java/com/hyundai/challenge/configuration/handler/error/ApplicationExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,12 +18,24 @@ public class ApplicationExceptionHandler {
         errorMap.put("status", HttpStatus.BAD_REQUEST.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap);
     }
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handlerGenericException(Exception exception){
+    private ResponseEntity<Map<String, String>> handler(Exception exception, HttpStatus httpStatus){
         Map<String, String> errorMap = new HashMap<>();
         exception.printStackTrace();
-        errorMap.put("error message", exception.getMessage());
-        errorMap.put("status", HttpStatus.INTERNAL_SERVER_ERROR.toString());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMap);
+        String msgException=exception.getMessage();
+        String msg = msgException.substring(msgException.indexOf("\"") + 1, msgException.lastIndexOf("\""));
+        errorMap.put("error message",msg);
+        errorMap.put("status", httpStatus.toString());
+
+        return ResponseEntity.status(httpStatus).body(errorMap);
     }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handlerGenericException(Exception exception){
+        return handler(exception, HttpStatus.INTERNAL_SERVER_ERROR) ;
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handlerResponseStatusException(ResponseStatusException exception){
+        return handler(exception,exception.getStatus()) ;
+    }
+
 }
